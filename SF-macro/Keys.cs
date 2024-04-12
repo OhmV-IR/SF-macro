9e384b66178e68ee6512161d2af596c5d729904b
Adrien B <bourdeaux.adrien@gmail.com>
2024-04-12 11:18:26 -0400

    Add project files.

---
+++ SF-macro/Keys.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static SF_macro.Inputs;
+
+namespace SF_macro
+{
+    public class Keys
+    {
+        public static void SendKeyPress(KeyCode keyCode)
+        {
+            INPUT input = new INPUT
+            {
+                Type = 1
+            };
+            input.Data.Keyboard = new KEYBDINPUT()
+            {
+                Vk = (ushort)keyCode,
+                Scan = 0,
+                Flags = 0,
+                Time = 0,
+                ExtraInfo = IntPtr.Zero,
+            };
+
+            INPUT input2 = new INPUT
+            {
+                Type = 1
+            };
+            input2.Data.Keyboard = new KEYBDINPUT()
+            {
+                Vk = (ushort)keyCode,
+                Scan = 0,
+                Flags = 2,
+                Time = 0,
+                ExtraInfo = IntPtr.Zero
+            };
+            INPUT[] inputs = new INPUT[] { input, input2 };
+            if (SendInput(2, inputs, Marshal.SizeOf(typeof(INPUT))) == 0)
+                Console.WriteLine("Failed to send inputs");
+        }
+    }
+}
---
+++ SF-macro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+namespace SF_macro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program started");
+            Thread.Sleep(2000);
+            var width = Screen.PrimaryScreen.WorkingArea.Width;
+            var height = Screen.PrimaryScreen.WorkingArea.Height;
+            Clicks.LeftClick(width / 2, height / 2);
+            Thread.Sleep(2000);
+            //Keys.SendKeyPress(Inputs.KeyCode.KEY_3);
+        }
+    }
+}
