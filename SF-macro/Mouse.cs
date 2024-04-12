0711645fa5578284615d117a15682542e50f1c44
Adrien B <bourdeaux.adrien@gmail.com>
2024-04-12 15:19:14 -0400

    my mouse keys were switched for some reason lmao what so the hex codes are actually right

--- SF-macro/Mouse.cs
+++ SF-macro/Mouse.cs
@@ -9,30 +9,27 @@ namespace SF_macro
     public class Mouse
     {
         // NOTES:
-        // The hex code for left click is supposed to be 0x02 and then 0x04 but they are flipped for some reason... 0x02 then 0x04 performs a right click
-        // and 0x08 then 0x10 performs a left click
         // The x and y coordinates given to the mouse_event method do nothing, weird dll stuff I assume
         public static void RightClick() {
-            mouse_event(0x02, 0, 0, 0, (UIntPtr)0);
-            mouse_event(0x04, 0, 0, 0, (UIntPtr)0);
+            mouse_event(0x08, 0, 0, 0, (UIntPtr)0);
+            mouse_event(0x10, 0, 0, 0, (UIntPtr)0);
         }
         public static void RightClick(int x, int y)
         {
             MoveMouse(x, y);
-            mouse_event(0x02, 0, 0, 0, (UIntPtr)0);
-            mouse_event(0x04, 0, 0, 0, (UIntPtr)0);
+            mouse_event(0x08, 0, 0, 0, (UIntPtr)0);
+            mouse_event(0x10, 0, 0, 0, (UIntPtr)0);
         }
         public static void LeftClick()
         {
-
-            mouse_event(0x08, 0, 0, 0, (UIntPtr)0);
-            mouse_event(0x10, 0, 0, 0, (UIntPtr)0);
+            mouse_event(0x02, 0, 0, 0, (UIntPtr)0);
+            mouse_event(0x04, 0, 0, 0, (UIntPtr)0);
         }
         public static void LeftClick(int x, int y)
         {
             MoveMouse(x, y);
-            mouse_event(0x08, 0, 0, 0, (UIntPtr)0);
-            mouse_event(0x10, 0, 0, 0, (UIntPtr)0);
+            mouse_event(0x02, 0, 0, 0, (UIntPtr)0);
+            mouse_event(0x04, 0, 0, 0, (UIntPtr)0);
         }
         public static void MoveMouse(int x, int y)
         {
